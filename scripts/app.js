984d2381a42e6f711242fce9d093d90d2acc0bd2
CodeyPacker <codeypacker@gmail.com>
2019-12-26 23:39:12 -0700

    Added shake to clear canvas

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakeButton = document.querySelector('.shake');
-const MOVE_AMOUNT = 15;
+const MOVE_AMOUNT = 30;
 
 // setup the canvas for drawing
 const { width, height } = canvas;
@@ -61,4 +61,15 @@ function handleKey(e) {
   }
 }
 
+// clear /shake function
+function clearCanvas() {
+  canvas.classList.add('shake');
+  ctx.clearRect(0, 0, width, height);
+  canvas.addEventListener('animationnd', function() {
+    canvas.classList.remove('shake');
+  }, { once: true }
+  )
+}
+
 window.addEventListener('keydown', handleKey);
+shakeButton.addEventListener('click', clearCanvas);
